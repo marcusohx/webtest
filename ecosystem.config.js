31140110e04675a1d88270bd8371ce0a6fa804aa
unknown <marcusoh3@gmail.com>
2018-10-17 10:36:47 +0800

    setup pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,18 @@
 module.exports = {
-  apps: [{
-    name: 'webapp',
-    script: './app.js'
+  apps : [{
+    name: 'test',
+    script: 'app.js',
   }],
-  deploy: {
-    production: {
-      user: 'ubuntu',
-      host: 'ec2-54-185-28-76.us-west-2.compute.amazonaws.com',
-      key: '~/.ssh/webapp.pem',
-      ref: 'origin/master',
-      repo: 'https://github.com/marcusohx/webtest',
-      path: '/home/ubuntu/webapp',
-      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+
+  deploy : {
+    production : {
+      user : 'ubuntu',
+      host : 'ec2-54-185-28-76.us-west-2.compute.amazonaws.com',
+      key  : '~/.ssh/test.pem',
+      ref  : 'origin/master',
+      repo : 'git@github.com:/marcusohx/webtest.git',
+      path : '/home/ubuntu/test',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
     }
   }
-}
+};
