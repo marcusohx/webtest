7af3220ae5654e31ae09f69c3a0cb6a6a1e890bc
unknown <marcusoh3@gmail.com>
2018-10-24 14:03:29 +0800

    ze server

--- app.js
+++ app.js
@@ -26,31 +26,98 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname,'public')))
 
 
-var users = [
-	{
-		id:'1',
-		first_name: 'john',
-		last_name:'doe',
-		email:'johndoe@gmail.com',
-	},
-	{
-		id:'2',
-		first_name: 'Bob',
-		last_name:'smith',
-		email:'bobsmith@gmail.com',
-	},
-	{
-		id:'3',
-		first_name: 'jill',
-		last_name:'jackson',
-		email:'jilljackson@gmail.com',
-	}
-]
-
-
 
 
 var server = net.createServer();
+
+let clients = {}
+
+let clientCount = 0;
+
+let deviceid = "1234";
+
+server.on('connection',function(connection){
+	let clientname
+
+	let message = [];
+
+	function broadcast( msg ){
+		  //Loop through the active clients object
+		  for( let user in clients ){
+		    //Send message to all active clients except yourself 
+		    if( clients[ user ] !== connection ){
+
+		    	clients[ user ].write(msg);
+		    }
+		}
+	}
+	connection.write(`Please enter a username\r\n`);
+	connection.setEncoding('utf-8');
+
+	connection.on('data',function(data){
+		message.push(data);
+		
+		let clientInput = message.join('').replace('\r\n','');
+		
+		if(!clientname){
+			
+			if( clients[clientInput]){
+				connection.write('device added already')
+				message = [];
+				return;
+			} else {
+				clientname = clientInput;
+				clientCount++;
+				clients[clientInput] = connection;
+				connection.write(`- Welcome to the Chatbox, There are ${clientCount} active users\r\n`);
+          		//Discard the previous keystrokes the client entered
+          		message = [];
+
+
+          	}
+          	} else {
+				//Send the message received to every client
+				if(clientname === deviceid){
+					broadcast(`${clientInput}`);
+	        	//Discard the previous keystrokes the client entered
+	        		message = [];
+				}
+				else{
+					console.log('This device is for recieving')
+				}
+
+
+	        }
+	        
+	    
+	});
+	//A close event is emitted when a connection has disconnected from the server
+	connection.on('close', () => {
+	    //When a client disconnects, remove the name and connection
+	    delete clients[clientname];
+	    //Decrease the active client count
+	    clientCount--;
+	    //Send a message to every active client that someone just left the chat
+	})
+
+	//Handle error events
+	connection.on('error', error => {
+		connection.write(`Error : ${error}`);
+	})
+})
+
+
+
+
+server.on('close',function(){
+	 //When a client disconnects, remove the name and connection
+	 delete clients[clientname];
+    //Decrease the active client count
+    clientCount--;
+    //Send a message to every active client that someone just left the chat
+    console.log(`server disconnected`);
+});
+
 
 /*
 var server = net.createServer(function(socket) {
@@ -61,25 +128,79 @@ var server = net.createServer(function(socket) {
 	})
 });
 
-*/
+
 
 var data = ""
 var remAdd = ""
+
+var deviceid = "1234"
+var deviceid2 = "5678"
+var correctdevice1 = ""
+var correctdevice2 = ""
+
+var clients = [];
 
 server.on("connection",function(socket){
 
 	var remoteAddress = socket.remoteAddress + ":" + socket.remotePort;
 	console.log("new client connection is made %s", remoteAddress)
 	
+	clients.push(socket);
+
+	console.log(clients);
+
 	socket.on("data",function(d){
+		
 		console.log("Data from %s: %s",remoteAddress,d)
-		data = d
-		remAdd = remoteAddress
+		console.log(d)
+		if(correctdevice1 == "send" && correctdevice2=="send"){
+			console.log("success connection of both device");
+			data = d.toString('utf8');
+			socket.write(data);
+			console.log(data); 
+		}else{
+			verifydevice1(d,deviceid)
+			verifydevice2(d,deviceid2)
+			console.log("connection unsuccessful")
+			
+		}
+
 	});
 	socket.once("close",function(){
 		console.log("connection closing %s",remoteAddress)
 	});
 })
+
+//hexx convertion
+
+function hex2a(hexx) {
+    var hex = hexx.toString();//force conversion
+    var str = '';
+    for (var i = 0; (i < hex.length && hex.substr(i, 2) !== '00'); i += 2)
+        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    return str;
+}
+
+function verifydevice1(connectiondata,device){
+	if(connectiondata.toString('utf8') === device){
+			console.log("success connection 1");
+			correctdevice1 = "send";
+			
+		}
+		else{
+			console.log("invalid device 1");
+		}
+}
+function verifydevice2(connectiondata,device){
+	if(connectiondata.toString('utf8') === device){
+			console.log("success connection 2");
+			correctdevice2 = "send";
+			
+		}
+		else{
+			console.log("invalid device 2");
+		}
+}
 
 app.get('/',function(req,res){
 	res.render('index',{
@@ -90,7 +211,7 @@ app.get('/',function(req,res){
 	});
 });
 
-
+*/
 server.listen(20000,function(){
 	console.log("server listening to port 20000")
 })
